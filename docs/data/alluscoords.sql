459e2e7890da210fcabae6d13363a2faddb6b4e5
Trenton <trentonmbailey@gmail.com>
2019-04-18 10:17:55 -0500

    Add Queries to the Data

--- docs/data/alluscoords.sql
+++ docs/data/alluscoords.sql
@@ -1,3 +1,5 @@
-SELECT s.latitude,s.longitude
+CREATE VIEW WeightedCoords AS
+SELECT s.latitude,s.longitude,count(s.latitude)
 FROM sighting s, location l
-WHERE l.country='US' AND s.latitude = l.Latitude AND s.longitude = l.Longitude;
+WHERE l.country='US' AND s.latitude = l.Latitude AND s.longitude = l.Longitude
+GROUP BY s.latitude,s.longitude;
